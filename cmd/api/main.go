5ad4f9f5e67f5da7237379c18dd7b07b63b1624e
ferneediaz <dandiaz10@yahoo.com>
2024-10-28 01:51:49 +0800

    configuring DB connections pool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/ferneediaz/gopher-socials/internal/db"
 	"github.com/ferneediaz/gopher-socials/internal/env"
 	"github.com/ferneediaz/gopher-socials/internal/store"
 )
@@ -10,10 +11,29 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store.NewStorage(nil)
+
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Printf("database connection pool established")
+	store := store.NewStorage(db)
 	app := &application{
 		config: cfg,
+		store:  store,
 	}
 
 	mux := app.mount()
