fea3db5f4dd47834ed1b68802ea592f1953de2d8
ferneediaz <dandiaz10@yahoo.com>
2024-10-29 02:10:14 +0800

    HTTP payload validation

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -12,13 +12,13 @@ func (app *application) internalServerError(w http.ResponseWriter, r *http.Reque
 }
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	log.Printf("bad request error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusBadRequest, err.Error())
 }
 
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal error: %s path: %s error: %s", r.Method, r.URL.Path, err)
+	log.Printf("not found error: %s path: %s error: %s", r.Method, r.URL.Path, err)
 
 	writeJSONError(w, http.StatusNotFound, "not found")
 }
