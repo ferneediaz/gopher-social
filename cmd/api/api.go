3689ddb69aedac2d209df450cc414e4f5f6c4cec
ferneediaz <dandiaz10@yahoo.com>
2024-11-06 08:07:46 +0800

    auto generating documentation Added swagger docs

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"fmt"
 
 	"github.com/ferneediaz/gopher-socials/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+	"github.com/ferneediaz/gopher-socials/docs"
 )
-
-const version = "0.0.1"
 
 type application struct {
 	config config
@@ -21,6 +22,7 @@ type config struct {
 	addr string
 	db   dbConfig
 	env  string
+	apiURL string
 }
 
 type dbConfig struct {
@@ -45,6 +47,9 @@ func (app *application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.addr)
+		r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(docsURL)))
 
 		r.Route("/posts", func(r chi.Router) {
 			r.Post("/", app.createPostHandler)
@@ -73,6 +78,10 @@ func (app *application) mount() http.Handler {
 	return r
 }
 func (app *application) run(mux http.Handler) error {
+	//Docs
+	docs.SwaggerInfo.Version = version
+	docs.SwaggerInfo.Host = app.config.apiURL
+	docs.SwaggerInfo.BasePath = "/v1"
 
 	srv := &http.Server{
 		Addr:         app.config.addr,
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,9 +8,29 @@ import (
 	"github.com/ferneediaz/gopher-socials/internal/store"
 )
 
+const version = "0.0.1"
+//	@title	Gopher Socials API
+
+//	@description	API FOR gopher social.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+//	@BasePath	/v1
+//
+//@securityDefinitions.api_key 	ApiKeyAuth
+//@in 				header
+//@name 			Authorization
+//@description 	    Authorization
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "http://localhost:8080"),
 		db: dbConfig{
 			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
